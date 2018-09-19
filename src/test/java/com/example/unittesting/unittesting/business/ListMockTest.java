ecf9830c27a4acef33bf254116cb0d16fd0016f0
luksen1991 <lukasz1991code@gmail.com>
2018-09-19 23:08:14 +0200

    Mock list testing

---
+++ src/test/java/com/example/unittesting/unittesting/business/ListMockTest.java
@@ -0,0 +1,49 @@
+package com.example.unittesting.unittesting.business;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListMockTest {
+
+
+    List mock = mock(List.class);
+
+    @Test
+    public void sizeBasic(){
+        when(mock.size()).thenReturn(5);
+        assertEquals(5,mock.size());
+    }
+
+    @Test
+    public void returnDiffrentValue(){
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5,mock.size());
+        assertEquals(10,mock.size());
+    }
+
+    @Test
+    public void returnWithParameters(){
+
+        when(mock.get(0)).thenReturn("HelloWorld");
+        assertEquals("HelloWorld",mock.get(0));
+        assertEquals(null,mock.get(1));
+    }
+
+    @Test
+    public void returnWithGenericParameters(){
+
+        when(mock.get(anyInt())).thenReturn("HelloWorld");
+        assertEquals("HelloWorld",mock.get(0));
+        assertEquals("HelloWorld",mock.get(1));
+        assertEquals(5,mock.size());
+    }
+
+
+
+}
