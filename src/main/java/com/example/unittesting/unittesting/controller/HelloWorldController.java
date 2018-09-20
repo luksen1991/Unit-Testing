81560693757b14cc85140007edc389bfc3831fdf
luksen1991 <lukasz1991code@gmail.com>
2018-09-20 10:34:01 +0200

    Add @RestController and testing helloworld with use MockMvc

---
+++ src/main/java/com/example/unittesting/unittesting/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.unittesting.unittesting.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+}
