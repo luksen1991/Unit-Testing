2de0f66cdf6d21cb3d6c5bf355b4c0927c3d2cc3
luksen1991 <lukasz1991code@gmail.com>
2018-09-21 02:16:02 +0200

    Add IntegrationTest

---
+++ src/test/java/com/example/unittesting/unittesting/controller/ItemControllerIT.java
@@ -0,0 +1,24 @@
+package com.example.unittesting.unittesting.controller;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ItemControllerIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getAll() throws JSONException {
+        String response = this.restTemplate.getForObject("/item/allItems",String.class);
+        JSONAssert.assertEquals("[{id:1},{id:2}]",response,false);
+    }
+}
